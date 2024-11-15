330c50d0c21d152e966bef67d46cdc54a164d8d4
nik-etl <niklas.etling@gmail.com>
2024-11-15 09:41:31 +0200

    Update main.py
    Added rough restaurant class and Hesburger view

--- project_foodApp/main.py
+++ project_foodApp/main.py
@@ -1,5 +1,42 @@
 import flet as ft
 
+# Create a restaurant class that has types of food, menu, and location
+# if easy implement search system for the menu
+class Restaurant:
+    def __init__(self, type_of_food: str, menu: dict, location: str, rating: float):
+        if rating < 0 or rating > 5:
+            raise ValueError("Rating must be between 0 and 5")
+        self.type_of_food = type_of_food
+        self.menu = menu  # menu should be a dictionary with the food as the key and the price as the value
+        self.location = location # location is a string that indicates where certain restaurant is located
+        # possibility for certain restaurants to only have certain locations
+        # ex: taco bell only in Helsinki
+        # user could select through autoselect
+        self.rating = rating
+        # get rating from yelp or google if possible implement user inputted rating
+    def add_item(self):
+        pass # add item to basket
+
+    
+    # def get_menu(self):
+    #     return self.menu
+    # def get_rating(self):
+    #     return self.rating
+    # def get_location(self):
+    #     return self.location
+    # def get_type_of_food(self):
+    #     return self.type_of_food
+    
+class Hesburger(Restaurant):
+    def __init__(self):
+        super().__init__()
+
+class TacoBell(Restaurant):
+    def __init__(self):
+        super().__init__()
+class Subway(Restaurant):
+    def __init__(self):
+        super().__init__()
 # This is the main function that will be executed when the app starts
 def main(page: ft.Page):
     # Variables used in the authentication pages
@@ -58,7 +95,6 @@ def main(page: ft.Page):
             signup_confirm_password.update()
             page.go("/homepage")
 
-    
     # Function to validate the sign in form
     def signin_validate(e):
         has_error = False
@@ -84,8 +120,8 @@ def main(page: ft.Page):
             signin_password.update()
             page.go("/homepage")
 
-    
 
+    # Function to change the navigation route
     def route_change(route):
         page.views.clear()
         page.views.append(
@@ -126,8 +162,25 @@ def main(page: ft.Page):
                     ],
                 )
             )
+        if page.route == "/hesburger":
+            page.views.append(
+                ft.View(
+                    "/hesburger",
+                    [
+                        ft.AppBar(title=ft.Text("Hesburger"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.Text("This is the Hesburger page"),
+                        ft.Placeholder(
+                            label="foodpicture",
+                            expand=True,
+                            color=ft.colors.random_color()
+                        ),
+                        ft.ElevatedButton("Back", on_click=lambda _: page.on_view_pop()),
+                    ],
+                )
+            )
         page.update()
     
+    # Function to go back to the previous view
     def view_pop(view):
         page.views.pop()
         top_view = page.views[-1]
